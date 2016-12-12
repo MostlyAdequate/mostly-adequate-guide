c6ea90dffcfdc8453f1bb8fcaa37168be93562bb
Takle <christian.takle@gmail.com>
2016-12-12 23:39:36 +0000

    App-demo: Remove whitespace

--- code/part1_demo/flickr.js
+++ code/part1_demo/flickr.js
@@ -10,48 +10,40 @@ require([
     'jquery'
   ],
   function (_, $) {
-    ////////////////////////////////////////////
-    // Utils
-    //
+    //-- Debug --------------------------------------------------------
+    var trace = _.curry(function(tag, x) {
+      console.log(tag, x);
+      return x;
+    })
+
+    //-- Utils --------------------------------------------------------
     var img = function (url) {
        return $('<img />', { src: url });
     };
 
     var Impure = {
       getJSON: _.curry(function(callback, url) {
-        $.getJSON(url, callback)
+        $.getJSON(url, callback);
       }),
 
       setHtml: _.curry(function(sel, html) {
-        $(sel).html(html)
+        $(sel).html(html);
       })
-    }
-
-    var trace = _.curry(function(tag, x) {
-        console.log(tag, x);
-        return x;
-    })
-
-    ////////////////////////////////////////////
-    // Pure
-
+    };
+    //-- Pure ---------------------------------------------------------
     //  url :: String -> URL
     var url = function (t) {
-      return 'https://api.flickr.com/services/feeds/photos_public.gne?tags=' + t + '&format=json&jsoncallback=?';
+      var query = '?tags='+t+'&format=json&jsoncallback=?';
+      return 'https://api.flickr.com/services/feeds/photos_public.gne'+query;
     };
 
     var mediaUrl = _.compose(_.prop('m'), _.prop('media'));
+    var srcs     = _.compose(_.map(mediaUrl), _.prop('items'));
+    var images   = _.compose(_.map(img), srcs);
 
-    var srcs = _.compose(_.map(mediaUrl), _.prop('items'));
+    //-- Impure -------------------------------------------------------
+    var renderImages = _.compose(Impure.setHtml('#js-app'), images);
+    var app          = _.compose(Impure.getJSON(renderImages), url);
 
-    var images = _.compose(_.map(img), srcs);
-
-
-    ////////////////////////////////////////////
-    // Impure
-    //
-    var renderImages = _.compose(Impure.setHtml("body"), images)
-    var app = _.compose(Impure.getJSON(renderImages), url)
-
-    app("cats")
+    app('cats');
   });
