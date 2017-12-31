f66ac5761a7a41b1aadb2e1aa85eb27b631508f6
KtorZ <matthias.benkort@gmail.com>
2017-12-31 15:15:45 +0100

    automate creation of new exercises template files

--- exercises/test-utils.js
+++ exercises/test-utils.js
@@ -1,5 +1,9 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
 const path = require('path');
+const cli = require('cli');
+const process = require('process');
 const { it } = require('mocha');
 const {
   compose,
@@ -18,6 +22,10 @@ const filenameRe = base => new RegExp(`${base}_([a-z]).js`);
 const readDir = d => fs.readdirSync(d);
 
 const readFile = f => fs.readFileSync(f).toString('utf-8');
+
+const writeFile = curry((f, data) => fs.writeFileSync(f, data));
+
+const mkdir = d => fs.mkdirSync(d);
 
 const mconcat = zero => reduce(concat, zero);
 
@@ -63,6 +71,86 @@ const runSolutions = compose(defineSpecs('solution'), readExercises);
 
 const runExercises = compose(defineSpecs('exercise'), readExercises);
 
+
+/* -------- Cli -------- */
+
+if (process.argv.length > 2 && process.argv[1] === __filename) {
+  const usage = `Usage: test-utils new
+
+Available options:
+  -h, --help                Show this help text
+  -c, --chapter             New chapter to create`;
+
+  const spec = curry((runner, chapter) => `const { describe } = require('mocha');
+const { ${runner} } = require('../test-utils');
+
+describe('Exercises Chapter ${chapter}', () => {
+  ${runner}('ch${chapter}');
+});`);
+
+  const addTestScript = curry((pkg, chapter) => {
+    const { scripts } = pkg;
+
+    return {
+      ...pkg,
+      scripts: {
+        ...scripts,
+        [`ch${chapter}`]: `mocha test/ch${chapter}.js`,
+      },
+    };
+  });
+
+  const failWith = curry((ctx, msg) => {
+    ctx.fatal([msg, usage].join('\n\n'));
+  });
+
+  // NOTE Override the `getUsage` method of the cli which is... not satisfactory.
+  cli.getUsage = () => { console.error(usage); cli.exit(0); }; // eslint-disable-line no-console
+
+  cli.parse({
+    chapter: ['c', 'New chapter to create', 'int'],
+  });
+
+  cli.main(function main(args, options) {
+    cli.failWith = failWith(this);
+
+    if (args.length !== 1 || args[0] !== 'new') {
+      cli.failWith(`Unknown command: ${args.join(' ')}`);
+    }
+
+    if (!options.chapter) {
+      cli.failWith('Missing required Chapter');
+    }
+
+    if (readDir(__dirname).includes(`ch${options.chapter}`)) {
+      cli.failWith(`Chapter ${options.chapter} already exists`);
+    }
+
+    mkdir(path.join(__dirname, `ch${options.chapter}`));
+
+    writeFile(
+      path.join(__dirname, 'test', `ch${options.chapter}.js`),
+      spec('runExercises', options.chapter),
+    );
+
+    writeFile(
+      path.join(__dirname, 'test', `ch${options.chapter}.solutions.js`),
+      spec('runSolutions', options.chapter),
+    );
+
+    const pkg = addTestScript(require('./package.json'), options.chapter); // eslint-disable-line global-require
+
+    writeFile(
+      path.join(__dirname, 'package.json'),
+      JSON.stringify(pkg, null, 2),
+    );
+
+    console.error(`Bootstrapped Chapter ${options.chapter}.`); // eslint-disable-line no-console
+  });
+}
+
+
+/* -------- Exports -------- */
 
 module.exports = {
   readExercises,
