fa6673d77880a65b85c75045cb0c45f6246b5460
ayeseeem <ayeseeem@gmail.com>
2018-03-07 10:21:32 +0000

    Remove spurious apostrophe

--- exercises/ch10/exercise_a.js
+++ exercises/ch10/exercise_a.js
@@ -1,4 +1,4 @@
-// Write a function that add's two possibly null numbers together using `Maybe` and `ap`.
+// Write a function that adds two possibly null numbers together using `Maybe` and `ap`.
 
 // safeAdd :: Maybe Number -> Maybe Number -> Maybe Number
 const safeAdd = undefined;
